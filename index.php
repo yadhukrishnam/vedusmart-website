f721646dbb78bba41b48f7c37deb7b6d16863cc9
Yadhu Krishna <yadhukrishna@anonymous.com>
2020-06-13 09:18:12 +0530

    Done

--- index.php
+++ index.php
@@ -1,22 +1,26 @@
 <?php include('includes/header.php'); ?>
-      <section class="welcome">
-          <div class="container">
-              <div class="row">
-                  <h1>WELCOME TO <span>V-EDUSMART</span></h1>
-                  <p>V-edu Smart Infotech (P) Ltd., is an organization specialized into management, Financial, Marketing and Consultancies. The Main objective of the <br> company is to promote  education products through various direct selling methods. V-edusmart proud to annouce that we have wide range of award <br> winning products at very attractive and  affordable products. All our producs are highly result oriented. We are sure that once you use any of our product you will be fully satisfied with that.</p>
-              </div>
-              <a href="#" class="green-btn">READ MORE</a>
-            </div>
+<section class="welcome">
+    <div class="container">
+        <div class="row">
+            <h1>WELCOME TO <span>V-EDUSMART</span></h1>
+            <p>V-edu Smart Infotech (P) Ltd., is an organization specialized into management, Financial, Marketing and
+                Consultancies. The Main objective of the <br> company is to promote education products through various
+                direct selling methods. V-edusmart proud to annouce that we have wide range of award <br> winning
+                products at very attractive and affordable products. All our producs are highly result oriented. We are
+                sure that once you use any of our product you will be fully satisfied with that.</p>
         </div>
-      </section>
-      <section class="main">
-          <div class="container-fluid">
-              <div class="content">
-                  <div class="products-heading">
-                   <h2>HOW IT WORKS</h2>
-                </div>
-                <div class="row">
-                  <div class="col-sm-4">
+        <a href="#" class="green-btn">READ MORE</a>
+    </div>
+    </div>
+</section>
+<section class="main">
+    <div class="container-fluid">
+        <div class="content">
+            <div class="products-heading">
+                <h2>HOW IT WORKS</h2>
+            </div>
+            <div class="row">
+                <div class="col-sm-4">
                     <div class="box">
                         <img src="images/grids-img1.jpg" />
                         <h3>PARENTS</h3>
@@ -28,114 +32,125 @@
                         </p>
                         <a href="#" class="green-btn">READ MORE</a>
                     </div>
-                  </div>
-                  <div class="col-sm-4">
-                      <div class="box">
-                          <img src="images/grids-img2.jpg" />
-                          <h3>TEACHERS</h3>
-                          <p>
-                            It enables the teachers to demonstrate, explain and reinforce abstract ideas by using animated pictures, charts, posters, graphics and motion videos.<br />
+                </div>
+                <div class="col-sm-4">
+                    <div class="box">
+                        <img src="images/grids-img2.jpg" />
+                        <h3>TEACHERS</h3>
+                        <p>
+                            It enables the teachers to demonstrate, explain and reinforce abstract ideas by using
+                            animated pictures, charts, posters, graphics and motion videos.<br />
                             It helps the teachers to evaluate the performance of each of their students.<br />
                             It enhance the reputation of teachers as it helps to avoid tuition.</p>
-                          <a href="#" class="green-btn">READ MORE</a>
-                      </div>
-                  </div>
-                  <div class="col-sm-4">
-                      <div class="box">
-                          <img src="images/grids-img3.jpg" />
-                          <h3>STUDENTS</h3>
-                          <p>
+                        <a href="#" class="green-btn">READ MORE</a>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="box">
+                        <img src="images/grids-img3.jpg" />
+                        <h3>STUDENTS</h3>
+                        <p>
                             Helps the students to build interest in learning the subjects<br />
                             Helps the students to remember topics learned.<br />
-                            It provides opportunity to understand and internalize the basic concepts through animated objects, pictures, graphics.<br />
+                            It provides opportunity to understand and internalize the basic concepts through animated
+                            objects, pictures, graphics.<br />
                             It shows practical applications and save time.<br></p>
-                          <a href="#" class="green-btn">READ MORE</a>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </section>
-      <?php
+                        <a href="#" class="green-btn">READ MORE</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+</section>
+<?php
           include('includes/software-product.php');
       ?>
-      <section class="news">
-          <div class="container">
-              <div class="row content">
-                  <div class="col-sm-7">
-                      <div class="products-heading">
-                         <h3>NEWS & EVENTS</h3>
-                      </div>
-                      <p>
-                      <marquee behavior="scroll" direction="up" onmouseover="this.stop();" onmouseout="this.start();">
-                          <ul>
-                                <li><span class="fa fa-thumbs-up"></span> We have opened the door of oppotunity to the public on 25.05.2020</li>
-                                <li><span class="fa fa-thumbs-up"></span> Rare opportunity in direct marketing</li>
-                                <li><span class="fa fa-thumbs-up"></span> Less work and high benefits.  </li>
-                                <li><span class="fa fa-thumbs-up"></span> 5 Star income benefits and chance to be part of company!</li>
-                                <li><span class="fa fa-thumbs-up"></span> Qualify easily and get into autopool.</li>
-                                <li><span class="fa fa-thumbs-up"></span> No more hassle of team building. </li>
-                                <li><span class="fa fa-thumbs-up"></span> Find best products at very nominal price!</li>
-                          </ul>
-                      </marquee>
-                      </p>
-                  </div>
-                  <div class="col-sm-5">
-                      <iframe width="420" height="315" class="video" src="https://www.youtube.com/embed/1OQLocWrOio">
-                      </iframe>
-                  </div>
-              </div>
-          </div>
-      </section>
-      <section class="achievers">
-          <div class="container">
-              <div class="content">
-                  <div class="products-heading">
-                   <h2>TWO STAR ACHIEVERS</h2>
+<section class="news">
+    <div class="container">
+        <div class="row content">
+            <div class="col-sm-7">
+                <div class="products-heading">
+                    <h3>NEWS & EVENTS</h3>
                 </div>
-                <div class="row">
-                  <div class="col-sm-2">
+                <p>
+                    <marquee behavior="scroll" direction="up" onmouseover="this.stop();" onmouseout="this.start();">
+                        <ul>
+                            <li><span class="fa fa-thumbs-up"></span> We have opened the door of oppotunity to the
+                                public on 25.05.2020</li>
+                            <li><span class="fa fa-thumbs-up"></span> Rare opportunity in direct marketing</li>
+                            <li><span class="fa fa-thumbs-up"></span> Less work and high benefits. </li>
+                            <li><span class="fa fa-thumbs-up"></span> 5 Star income benefits and chance to be part of
+                                company!</li>
+                            <li><span class="fa fa-thumbs-up"></span> Qualify easily and get into autopool.</li>
+                            <li><span class="fa fa-thumbs-up"></span> No more hassle of team building. </li>
+                            <li><span class="fa fa-thumbs-up"></span> Find best products at very nominal price!</li>
+                        </ul>
+                    </marquee>
+                </p>
+            </div>
+            <div class="col-sm-5">
+                <iframe width="420" height="315" class="video" src="https://www.youtube.com/embed/1OQLocWrOio">
+                </iframe>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="achievers">
+    <div class="container">
+        <div class="content">
+            <div class="products-heading">
+                <h2>TWO STAR ACHIEVERS</h2>
+            </div>
+            <div class="row">
+                <div class="col-sm-2">
                     <div class="box">
                         <img src="images/w1.jpg" />
                         <h6>Harinath</h6>
                     </div>
-                  </div>
-                  <div class="col-sm-2">
-                      <div class="box">
-                          <img src="images/w2.jpg" />
-                          <h6>Dileep Kumar</h6>
-                      </div>
-                  </div>
-                  <div class="col-sm-2">
-                      <div class="box">
-                          <img src="images/w3.jpg" />
-                          <h6>Karunakar Rao</h6>
-                      </div>
-                  </div>
-                   <div class="col-sm-2">
-                      <div class="box">
-                          <img src="images/w4.jpg" />
-                          <h6>Denton Gamble</h6>
-                      </div>
-                  </div>
-				           <div class="col-sm-2">
-                      <div class="box">
-                          <img src="images/w5.jpg" />
-                          <h6>Ashar Rushdie</h6>
-                      </div>
-                  </div>
-        				  <div class="col-sm-2">
-                              <div class="box">
-                                  <img src="images/w6.jpg" />
-                                  <h6>Rashid</h6>
-                              </div>
-                          </div>
-        				  <div class="col-sm-2">
-                              <div class="box">
-                                  <img src="images/w7.jpg" />
-                                  <h6>Hameed</h6>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-      </section>
+                </div>
+                <div class="col-sm-2">
+                    <div class="box">
+                        <img src="images/w2.jpg" />
+                        <h6>Dileep Kumar</h6>
+                    </div>
+                </div>
+                <div class="col-sm-2">
+                    <div class="box">
+                        <img src="images/w3.jpg" />
+                        <h6>Karunakar Rao</h6>
+                    </div>
+                </div>
+                <div class="col-sm-2">
+                    <div class="box">
+                        <img src="images/w4.jpg" />
+                        <h6>Denton Gamble</h6>
+                    </div>
+                </div>
+                <div class="col-sm-2">
+                    <div class="box">
+                        <img src="images/w5.jpg" />
+                        <h6>Ashar Rushdie</h6>
+                    </div>
+                </div>
+                <div class="col-sm-2">
+                    <div class="box">
+                        <img src="images/w6.jpg" />
+                        <h6>Rashid</h6>
+                    </div>
+                </div>
+                <div class="col-sm-2">
+                    <div class="box">
+                        <img src="images/w7.jpg" />
+                        <h6>Hameed</h6>
+                    </div>
+                </div>
+                <div class="col-sm-2">
+                    <a href="#">
+                        <div class="more-circle">
+                            <h2 class="fa fa-angle-right"></h2>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+</section>
 <?php include ('includes/footer.php'); ?>
